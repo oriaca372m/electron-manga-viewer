90930c197a2e1b6ad9115883d4f1a724ea8ac2f0
oriaca <oriaca372m@outlook.jp>
2021-07-28 18:20:44 +0900

    最小限動く

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -54,6 +54,135 @@ async function loadManga(path: string): Promise<MangaView> {
 	return mangaView
 }
 
+interface Point {
+	x: number
+	y: number
+}
+
+interface Rect {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+function renderedRect(
+	imgW: number,
+	imgH: number,
+	viewW: number,
+	viewH: number
+): Rect & { ratio: number } {
+	const wRatio = viewW / imgW
+	const hRatio = viewH / imgH
+	const ratio = Math.min(wRatio, hRatio)
+	const width = ratio * imgW
+	const height = ratio * imgH
+	return { ratio, width, height, x: (viewW - width) / 2, y: (viewH - height) / 2 }
+}
+
+function twoPointToRect(p1: Point, p2: Point): Rect {
+	const x = Math.min(p1.x, p2.x)
+	const y = Math.min(p1.y, p2.y)
+	const width = Math.abs(p1.x - p2.x)
+	const height = Math.abs(p1.y - p2.y)
+	return { x, y, width, height }
+}
+
+class RegionSelector {
+	#p1: Point = { x: 0, y: 0 }
+	#p2: Point = { x: 0, y: 0 }
+	#isEnabled = true
+
+	// とりあえず
+	private canvas = document.getElementById('view') as HTMLCanvasElement
+
+	constructor(readonly elm: HTMLDivElement) {}
+
+	setup(): void {
+		document.addEventListener('keydown', (e) => {
+			if (!this.#isEnabled) {
+				return
+			}
+
+			console.log(e.code)
+			if (e.code === 'Enter') {
+				console.log('enter')
+				void this.capture()
+			}
+		})
+	}
+
+	#syncElm(): void {
+		const rect = twoPointToRect(this.#p1, this.#p2)
+		this.elm.style.left = `${rect.x}px`
+		this.elm.style.top = `${rect.y}px`
+		this.elm.style.width = `${rect.width}px`
+		this.elm.style.height = `${rect.height}px`
+		this.elm.style.display = this.#isEnabled ? 'block' : 'none'
+	}
+
+	enable(): void {
+		this.#isEnabled = true
+		this.#syncElm()
+	}
+
+	disable(): void {
+		this.#isEnabled = false
+		this.#syncElm()
+	}
+
+	toggle(): void {
+		this.#isEnabled = !this.#isEnabled
+		this.#syncElm()
+	}
+
+	onSelectStart(x: number, y: number): void {
+		this.#p1 = { x, y }
+		this.#p2 = { x, y }
+		this.#syncElm()
+	}
+
+	onSelect(x: number, y: number): void {
+		this.#p2 = { x, y }
+		this.#syncElm()
+	}
+
+	onSelectEnd(x: number, y: number): void {
+		this.onSelect(x, y)
+	}
+
+	async capture(): Promise<void> {
+		const canvas = this.canvas
+		const canvasRect = canvas.getBoundingClientRect()
+		const rendered = renderedRect(
+			canvas.width,
+			canvas.height,
+			canvasRect.width,
+			canvasRect.height
+		)
+
+		const r = 1 / rendered.ratio
+		const rect = twoPointToRect(this.#p1, this.#p2)
+
+		const x = (rect.x - rendered.x) * r
+		const y = (rect.y - rendered.y) * r
+		const w = rect.width * r
+		const h = rect.height * r
+
+		const img = new OffscreenCanvas(w, h)
+		const ctx = img.getContext('2d')
+		if (ctx === null) {
+			throw new Error('error')
+		}
+		ctx.drawImage(canvas, x, y, w, h, 0, 0, w, h)
+
+		this.disable()
+
+		const blob = await img.convertToBlob({ type: 'image/png' })
+		await fs.writeFile('test.png', new Uint8Array(await blob.arrayBuffer()))
+	}
+}
+
 async function main() {
 	let mangaView: MangaView | undefined
 	try {
@@ -67,6 +196,11 @@ async function main() {
 
 	const loupeElm = document.getElementById('loupe') as HTMLCanvasElement
 	const loupe = new Loupe(loupeElm)
+
+	const regionSelector = new RegionSelector(
+		document.getElementById('region-selector') as HTMLDivElement
+	)
+	regionSelector.setup()
 
 	if (mangaView !== undefined) {
 		genThumbnails(mangaView)
@@ -100,6 +234,10 @@ async function main() {
 		}
 		if (e.code === 'ArrowRight') {
 			void mangaView?.prevPage(e.shiftKey)
+		}
+
+		if (e.key === 's') {
+			regionSelector.toggle()
 		}
 	})
 
@@ -174,11 +312,31 @@ async function main() {
 		const ry = e.clientY - viewRect.y
 		loupe.drawLoupe(rx, ry)
 		e.preventDefault()
+
+		if (e.buttons & 2) {
+			regionSelector.onSelectStart(rx, ry)
+		}
 	})
 
 	judge.addEventListener('mouseup', (e) => {
 		loupe.off()
 		e.preventDefault()
+
+		if (e.buttons & 2) {
+			const viewRect = judge.getBoundingClientRect()
+			const rx = e.clientX - viewRect.x
+			const ry = e.clientY - viewRect.y
+			regionSelector.onSelectEnd(rx, ry)
+		}
+	})
+
+	judge.addEventListener('mousemove', (e) => {
+		if (e.buttons & 2) {
+			const viewRect = judge.getBoundingClientRect()
+			const rx = e.clientX - viewRect.x
+			const ry = e.clientY - viewRect.y
+			regionSelector.onSelect(rx, ry)
+		}
 	})
 }
 
